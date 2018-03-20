a07e4ad85cea55a3dbed590d7ff7a3f2aad28546
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-20 17:37:17 +0300

    several ways to send html

--- server.js
+++ server.js
@@ -9,9 +9,28 @@ const app = express();
 const jsonData = {count: 12, message: 'hey'};
 
 app.get('/', (req, res) => {
-  fs.readFile('./index.html', (err, html) => {
+
+  //*** one way */
+
+  // res.sendFile(__dirname + '/index.html', (err) => {
+  //   if (err) res.status(500).send(err)
+  // })
+
+  
+  //*** or the other  */
+
+  // fs.readFile('./index.html', (err, html) => {
+  //   if (err) throw err;
+  //   res.end(html)
+  // })
+
+
+  //*** or even one else */
+
+  fs.readFile('./index.html', (err, buffer) => {
     if (err) throw err;
-    res.end(html)
+    res.setHeader('Content-Type', 'text/html')
+    res.send(buffer.toString())
   })
 });
 
