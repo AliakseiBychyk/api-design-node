941fb77ce38d04e7d57a9ce6a4e7e132149d8b7b
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-03-20 17:23:41 +0300

    exercise 1

--- server.js
+++ server.js
@@ -2,4 +2,22 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+import express from 'express';
+import fs from 'fs';
+
+const app = express();
+const jsonData = {count: 12, message: 'hey'};
+
+app.get('/', (req, res) => {
+  fs.readFile('./index.html', (err, html) => {
+    if (err) throw err;
+    res.end(html)
+  })
+});
+
+app.get('/data', (req, res) => {
+  res.json(jsonData);
+});
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
